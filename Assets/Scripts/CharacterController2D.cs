9fd87c51ae4b28c7235a48551c071fe20c1368d6
Audxius <audxius@gmail.com>
2023-02-03 22:16:31 +0200

    walking side to side

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    public bool Player2;
+    CapsuleCollider2D mainCollider;
+    Rigidbody2D rb;
+    public float maxSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        mainCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) && Mathf.Abs(rb.velocity.x) < maxSpeed)
+        {
+
+            rb.AddForce(new Vector2(-700, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) && Mathf.Abs(rb.velocity.x) < maxSpeed)
+        {
+
+            rb.AddForce(new Vector2(700, 0) * Time.deltaTime);
+        }
+    }
+}
