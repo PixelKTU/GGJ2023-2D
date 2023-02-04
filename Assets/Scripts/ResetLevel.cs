ad5ab88dd6e976cb2f6cff198f086d910e7d10f5
Arvydas <arvydas.mazauskas@ktu.edu>
2023-02-04 14:15:45 +0200

    tilemap

---
+++ Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ResetLevel : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
